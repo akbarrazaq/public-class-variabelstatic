605425a887af874a998a2e1186e23ee9a801c457
akbarrazaq <32825518+akbarrazaq@users.noreply.github.com>
2017-11-08 05:27:54 +0700

    Add files via upload

---
+++ public class variabelstatic.java
@@ -0,0 +1,14 @@
+public class variabelstatic {
+
+
+public static string nama;
+ 
+private string namaprivate;
+
+public static void main (string [] args){
+variabelstatic var = new variabelstatic [];
+var.nama private = "francius";
+variabelstatic.nama ="Francius";
+}
+	
+}
